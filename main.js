c47acb0d624f65d4d0e048215a677e7687eeb04f
dapuck <imccall@da-puck.com>
2013-06-22 17:34:38 -0500

    Figured out what was wrong with y control. While the origin for setting the mouse y coord is the upper left corner, the location reported by NSEvent is y zeroed in the lower left. This means that inorder to set the position for a delta change, we have to take the height of the screen into account. Still need to find a way to dynamicly discover this. For now, it is hard set for my screen size.

--- main.js
+++ main.js
@@ -2,10 +2,22 @@ var objc = require('NodObjC');
     objc.import('CoreGraphics');
     objc.import('Cocoa');
 
+objc.NSAutoreleasePool('alloc')('init');
+
+// get height of screen[0]. We will need this for move delta
+//var scrs = objc.NSScreen('screens');
+//var scr = objc.NSScreen('mainScreen'); //scrs('objectAtIndex', 0);
+//console.log(scr.ivars());
+var height = 768; //scrs[0].frame.size.height;
+//console.log(height);
+
 function mouseMoveDelta(dx,dy) {
 	// get the current location of the mouse
 	var pt = objc.NSEvent('mouseLocation');
-	objc.CGDisplayMoveCursorToPoint(objc.CGMainDisplayID(),objc.CGPointMake((pt.x + dx), (pt.y + dy)));
+	var x = pt.x + dx;
+	var y = (height - pt.y) + dy;
+	//objc.CGDisplayMoveCursorToPoint(objc.CGMainDisplayID(),objc.CGPointMake(x, y));
+	mouseMoveABS(x,y);
 }
 
 function mouseMoveABS(x,y) {
@@ -16,10 +28,16 @@ function showMouse() {
 	objc.CGDisplayShowCursor(objc.CGMainDisplayID());
 }
 
+function getCurrentPosition() {
+	var pt = objc.NSEvent('mouseLocation');
+	return {x:pt.x,y:pt.y};
+}
+
 module.exports = {
     "moveDelta": mouseMoveDelta,
     "moveABS": mouseMoveABS,
     "buttonDown": function(){},
     "buttonUp": function(){},
-    "show": showMouse
+    "show": showMouse,
+    "getCurrentPosition": getCurrentPosition
 }

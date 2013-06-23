84777ea52a8a56112a28a7c12231ffe1a49b085b
dapuck <imccall@da-puck.com>
2013-06-22 23:03:03 -0500

    Looked at code for synergy and found that what I was missing was CGEventPost. Moving the mouse and mouse down/up all work!

--- main.js
+++ main.js
@@ -3,19 +3,29 @@ var objc = require('NodObjC');
 	objc.import('ApplicationServices');
     objc.import('AppKit');
 
-var evtSrc = objc.CGEventSourceCreate(objc.kCGEventSourceStateHIDSystemState);
+var scr = objc.CGDisplayBounds(objc.CGMainDisplayID());
+var height = scr.size.height;
+var evtSource = objc.CGEventSourceCreate(objc.kCGEventSourceStateHIDSystemState);
 
 function mouseMoveDelta(dx,dy) {
 	// get the current location of the mouse
-	var pt = objc.NSEvent('mouseLocation');
-	var evt = objc.CGEventCreateMouseEvent(evtSrc, objc.kCGEventMouseMoved, objc.CGPointMake((pt.x + dx), (pt.y + dy)),objc.kCGMouseButtonLeft);
+	var pt = getCurrentPosition();
+	var x = pt.x + dx;
+	var y = pt.y + dy;
+	mouseMoveABS(x,y);
+	//var evt = objc.CGEventCreateMouseEvent(evtSrc, objc.kCGEventMouseMoved, objc.CGPointMake((pt.x + dx), (pt.y + dy)),objc.kCGMouseButtonLeft);
 	//objc.CFRelease(evt);
 	//objc.CGDisplayMoveCursorToPoint(objc.CGMainDisplayID(),objc.CGPointMake((pt.x + dx), (pt.y + dy)));
 }
 
 function mouseMoveABS(x,y) {
-	debugger;
-	var evt = objc.CGEventCreateMouseEvent(evtSrc, objc.kCGEventMouseMoved, objc.CGPointMake(x, y),objc.kCGMouseButtonLeft);
+	var evt = objc.CGEventCreateMouseEvent(
+		null, //evtSource, 
+		objc.kCGEventMouseMoved, 
+		objc.CGPointMake(x, y),
+		objc.kCGMouseButtonLeft
+	);
+	objc.CGEventPost(objc.kCGHIDEventTap, evt);
 	//objc.CFRelease(evt);
 	//objc.CGDisplayMoveCursorToPoint(objc.CGMainDisplayID(),objc.CGPointMake(x, y));
 }
@@ -24,10 +34,40 @@ function showMouse() {
 	objc.CGDisplayShowCursor(objc.CGMainDisplayID());
 }
 
+function getCurrentPosition() {
+	var pt = objc.NSEvent('mouseLocation');
+	return {x:pt.x,y:(height - pt.y)};
+}
+
+function mouseBtnDown() {
+	var pt = getCurrentPosition();
+	//console.log("down: ", pt);
+	var evt = objc.CGEventCreateMouseEvent(
+		evtSource, // event source
+		objc.kCGEventLeftMouseDown, // event type
+		objc.CGPointMake(pt.x,pt.y), // cursor position
+		objc.kCGMouseButtonLeft
+	);
+	objc.CGEventPost(objc.kCGHIDEventTap, evt);
+}
+
+function mouseBtnUp() {
+	var pt = getCurrentPosition();
+	//console.log("up: ", pt);
+	var evt = objc.CGEventCreateMouseEvent(
+		evtSource, // event source
+		objc.kCGEventLeftMouseUp, // event type
+		objc.CGPointMake(pt.x,pt.y), // cursor position
+		objc.kCGMouseButtonLeft
+	);
+	objc.CGEventPost(objc.kCGHIDEventTap, evt);
+}
+
 module.exports = {
     "moveDelta": mouseMoveDelta,
     "moveABS": mouseMoveABS,
-    "buttonDown": function(){},
-    "buttonUp": function(){},
-    "show": showMouse
+    "buttonDown": mouseBtnDown,
+    "buttonUp": mouseBtnUp,
+    "show": showMouse,
+    "getCurrentPosition": getCurrentPosition
 }
--- test.js
+++ test.js
@@ -1,25 +1,34 @@
-debugger;
 var mouse = require('./main');
 
 mouse.moveABS(50,50);
 mouse.show();
-console.log("50,50");
+console.log("should be: 50,50");
+var pt = mouse.getCurrentPosition();
+console.log("is: " + pt.x + "," + pt.y);
 setTimeout(function() {
 	mouse.moveDelta(50,0);
 	mouse.show();
-	console.log("100,50");
+	console.log("should be: 100,50");
+	pt = mouse.getCurrentPosition();
+	console.log("is: " + pt.x + "," + pt.y);
 	setTimeout(function() {
 		mouse.moveDelta(0,50);
 		mouse.show();
-		console.log("100,100");
+		console.log("should be: 100,100");
+		pt = mouse.getCurrentPosition();
+		console.log("is: " + pt.x + "," + pt.y);
 		setTimeout(function() {
 			mouse.moveDelta(-50,0);
 			mouse.show();
-			console.log("50,100");
+			console.log("should be: 50,100");
+			pt = mouse.getCurrentPosition();
+			console.log("is: " + pt.x + "," + pt.y);
 			setTimeout(function() {
 				mouse.moveDelta(0,-50);
 				mouse.show();
-				console.log("50,50");
+				console.log("should be: 50,50");
+				pt = mouse.getCurrentPosition();
+				console.log("is: " + pt.x + "," + pt.y);
 			},2000);
 		},2000);
 	},2000);
